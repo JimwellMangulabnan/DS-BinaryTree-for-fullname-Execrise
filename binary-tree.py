2ccf077ae96fc0e6be1883a7cec30d1fbae7977a
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-01-15 12:11:06 +0800

    creating class for binarysearchtreenode

---
+++ binary-tree.py
@@ -0,0 +1,12 @@
+print("\n\t*********  PROGRAMMED BY  ********")
+print("\t***** JIMWELL L. MANGULABNAN *****")
+print("\t********** BSCOE 2-2 *************")
+print()
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    
